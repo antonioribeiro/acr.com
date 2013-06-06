9f9fd01bd4b53532588b7b9929c36bcba17473f5
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2013-06-06 20:11:56 -0300

    removing aws.

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <?php
+	function curPageURL() {
+		$pageURL = 'http';
+		if (isset($_SERVER["HTTPS"]) and $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+		$pageURL .= "://";
+		if ($_SERVER["SERVER_PORT"] != "80") {
+		  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+		} else {
+		  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+		}
 
-  function curPageURL() {
-	$pageURL = 'http';
-	if (isset($_SERVER["HTTPS"]) and $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
-	$pageURL .= "://";
-	if ($_SERVER["SERVER_PORT"] != "80") {
-	  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-	} else {
-	  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+		return $pageURL;
 	}
 
-	return $pageURL;
-  }
+	function replace($smug, $url) {
+		$url = str_replace("www.antoniocarlosribeiro.com", $smug, $url);
+		$url = str_replace("antoniocarlosribeiro.com", $smug, $url);
+		$url = str_replace("172.17.0.2/consultoriu.com/acr.php", $smug, $url);
+		return $url;
+	}
 
-  function replace($smug, $url) {
-	$url = str_replace("www.antoniocarlosribeiro.com", $smug, $url);
-	$url = str_replace("antoniocarlosribeiro.com", $smug, $url);
-	$url = str_replace("172.17.0.2/consultoriu.com/acr.php", $smug, $url);
-	return $url;
-  }
-
-  // var_dump( replace("antoniocarlosribeiro.smugmug.com", curPageURL()) );
-  // die;
+	// var_dump( replace("antoniocarlosribeiro.smugmug.com", curPageURL()) );
+	// die;
 ?>
 
 <!DOCTYPE HTML>
