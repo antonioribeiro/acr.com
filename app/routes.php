7f3c29ebad550327e21d5936ddf73e47dd4293d1
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-01-06 12:06:47 -0200

    testing github hooks

--- app/routes.php
+++ app/routes.php
@@ -35,7 +35,7 @@ Route::get('deploy', function()
     Log::info(Input::all());
     Log::info("2");
     Log::info("3");
-    Log::info("12");
+    Log::info("13");
 
     return Deeployer::run();
 });
