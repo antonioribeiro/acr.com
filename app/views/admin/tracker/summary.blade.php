bd32506459c304a60099823ff350d1527cc3b7c7
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-06-04 16:18:05 -0300

    developing

--- app/views/admin/tracker/summary.blade.php
+++ app/views/admin/tracker/summary.blade.php
@@ -36,14 +36,16 @@
 	$(function()
     {
 		// Create a function that will handle AJAX requests
-		function requestData(days, chart){
+		function requestData(chart)
+		{
 			$.ajax({
 				type: "GET",
 				url: "{{url('admin/tracker/api/pageviews')}}", // This is the URL to the API
-				data: { days: days }
+				data: { }
 			})
 			.done(function( data ) {
 				// When the response to the AJAX request comes back render the chart with new data
+				console.log(data);
 				chart.setData(JSON.parse(data));
 			})
 			.fail(function() {
@@ -63,7 +65,7 @@
 		});
 
 		// Request initial data for the past 7 days:
-		requestData(7, chart);
+		requestData(chart);
 
 		$('ul.ranges a').click(function(e){
 			e.preventDefault();
