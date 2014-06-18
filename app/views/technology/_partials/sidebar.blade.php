e00592c9cfb8c2dc2d4d17196d427d6a8443d1e5
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-06-17 21:57:49 -0300

    developing

--- app/views/technology/_partials/sidebar.blade.php
+++ app/views/technology/_partials/sidebar.blade.php
@@ -1,7 +1,5 @@
 <p><a href="{{ URL::route('technology') }}/about me">{{g('About Me')}}</a></p>
 
-<p><a href="{{ URL::route('technology') }}/php">{{g('PHP & Laravel')}}</a></p>
-
 <p><a href="{{ URL::route('technology') }}/projects">{{g('Projects')}}</a></p>
 
 <hr>
