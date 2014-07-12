50fcfc3b1ff2dde284b886505192da59c9c9a1a9
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-07-12 19:42:23 -0300

    developing

--- app/config/packages/heybigname/backup-manager/storage.php
+++ app/config/packages/heybigname/backup-manager/storage.php
@@ -5,13 +5,13 @@ return [
         'type' => 'Local',
 
         'root' => getenv('BACKUP.PATH') . '/' .
-	                getenv('DOMAIN') . '/' .
-	                gethostname() . '.' .
-	                getenv('LARAVEL_ENV') . '.' .
-                    app()->make('db')->connection()->getDatabaseName() . '.' .
-	                app()->make('db')->connection()->getDriverName() . '.' .
-	                \Carbon\Carbon::now()->format('Y-m-d+H\hi\ms\s') . '.'
+                    getenv('DOMAIN'),
+
+        'prefix' => gethostname() . '.' .
+					getenv('LARAVEL_ENV') . '.' .
+					app()->make('db')->connection()->getDatabaseName() . '.' .
+					app()->make('db')->connection()->getDriverName() . '.' .
+                    \Carbon\Carbon::now()->format('Y-m-d+H\hi\ms\s') . '.' ,
 
     ],
 ];
-
